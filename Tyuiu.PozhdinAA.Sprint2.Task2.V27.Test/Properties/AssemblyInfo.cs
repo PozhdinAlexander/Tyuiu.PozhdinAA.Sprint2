4445a925e53125f1c3a76580d6ad04c5552f0dcc
Tyuiu.PozhdinAA@outlook.com <strimerkiy@bk.ru>
2023-10-05 23:31:24 +0500

    Task2

---
+++ Tyuiu.PozhdinAA.Sprint2.Task2.V27.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PozhdinAA.Sprint2.Task2.V27.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PozhdinAA.Sprint2.Task2.V27.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b1f49d63-b9bf-44ca-9c9a-a663e3a161ad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
