3573df9b41d0e3038c241215c740277cd579f72d
Tyuiu.PozhdinAA@outlook.com <strimerkiy@bk.ru>
2023-10-11 13:46:30 +0500

    Task3

---
+++ Tyuiu.PozhdinAA.Sprint2.Task3.V25.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PozhdinAA.Sprint2.Task3.V25.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PozhdinAA.Sprint2.Task3.V25.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9bbf1e35-e625-493f-93ad-bb59ec3adef0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
