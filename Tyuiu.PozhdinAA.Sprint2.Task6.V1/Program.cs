a3006ba3e7ad0614fd759bf7542b6849e3244585
Tyuiu.PozhdinAA@outlook.com <strimerkiy@bk.ru>
2023-10-13 01:06:59 +0500

    Task6

---
+++ Tyuiu.PozhdinAA.Sprint2.Task6.V1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PozhdinAA.Sprint2.Task6.V1.Lib;
+
+namespace Tyuiu.PozhdinAA.Sprint2.Task6.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Пождин А. А. | АСОиУБ-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы разветвляющейся структуры                               *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Пождин Александр Андреевич | АСОиУБ-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch вычисляет        *");
+            Console.WriteLine("* требуемое значение и возвращает результат.                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите номер месяца: ");
+            int numMonth = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+            if ((numMonth < 1) || (numMonth > 12))
+            {
+                res = "Введено неверное значение!";
+            }
+            else
+            {
+                res = "В этом месяце " + ds.FindMonthDaysCount(numMonth) + " дней";
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
